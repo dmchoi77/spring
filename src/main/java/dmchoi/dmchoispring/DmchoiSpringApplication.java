e7475e9d02a17356fb07e5fa14977739e6685ed5
dmchoi77 <dm.choi.dev@gmail.com>
2023-08-16 01:50:30 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/dmchoi/dmchoispring/DmchoiSpringApplication.java
@@ -0,0 +1,13 @@
+package dmchoi.dmchoispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmchoiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmchoiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmchoi/dmchoispring/DmchoiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dmchoi.dmchoispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmchoiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
