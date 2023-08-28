436cc2f355c8570441d3a940bdb8fb19cdec7d24
dmchoi77 <dm.choi.dev@gmail.com>
2023-08-28 21:43:49 +0900

    update: 컴포넌트 스캔 스프링빈 등록

--- src/main/java/dmchoi/dmchoispring/service/MemberService.java
+++ src/main/java/dmchoi/dmchoispring/service/MemberService.java
@@ -3,13 +3,17 @@ package dmchoi.dmchoispring.service;
 import dmchoi.dmchoispring.domain.Member;
 import dmchoi.dmchoispring.repository.MemberRepository;
 import dmchoi.dmchoispring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
